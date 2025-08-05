ef3dd4adc6a5a6616b9f28f519b7e837250078d9
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-05 10:38:14 +0600

    add the fecth system

--- javascript/sort.js
+++ javascript/sort.js
@@ -8,6 +8,29 @@ let javascript = `
                                                                             
 `
 // console.log(javascript);
+const fs = require('fs');
+
+
+// fetch the data from json file
+
+let JsonData;
+
+fs.readFile('../DB/data.json', 'utf8', (err, jsonString) => {
+     if (err) {
+          console.log("❎ Error Reading File ",err);
+          return null;
+     }
+     try{
+          const data = JSON.parse(jsonString);
+          console.log("Fetch data :" , data);
+          JsonData = data
+          
+     }catch(error){
+          console.error("❎ Error is coming :", error);
+          
+     }
+});
+
 
 
 
